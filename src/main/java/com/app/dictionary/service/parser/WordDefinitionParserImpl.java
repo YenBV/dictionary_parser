d65e30b4f8e1615c292984d5beda61311b039095
Yen Bach <bachvietyen00@gmail.com>
2021-05-10 14:22:01 +0300

    Implement equal and idiosyncratic meanings

--- src/main/java/com/app/dictionary/service/parser/WordDefinitionParserImpl.java
+++ src/main/java/com/app/dictionary/service/parser/WordDefinitionParserImpl.java
@@ -12,29 +12,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 @Service
 public class WordDefinitionParserImpl implements WordDefinitionParser {
 
     /**
-     * {*.}[word_definition]:[example]-[equivalent_definitions_separated_with_comma].
+     * 0-9[idiosyncraticMeaning].[equalMeaning][word_definition]:[example]-[equivalent_definitions_separated_with_comma].
      */
-    private final Pattern DEFINITION_EXAMPLE_EQUIVALENT_DEFINITIONS = Pattern.compile("\\\\*?\\.?\\s*(.*)[0-9]?:([^-]*)\\s*-?\\s*(.*)");
+    private final Pattern DEFINITION_EXAMPLE_EQUIVALENT_DEFINITIONS = Pattern.compile("[0-9]?\\s*(\\*?)\\.?\\s*(=?)\\s*(.*)[0-9]?:\\s*([^-]*)\\s*-?\\s*(.*)");
 
     @Override
     public WordDefinition parse(String wordDefinitionStr) {
         WordDefinition wordDefinition = new WordDefinition();
-        if (wordDefinitionStr.startsWith("*")) {
-            wordDefinition.setIdiosyncraticMeaning(true);
-        }
         Matcher matcher = DEFINITION_EXAMPLE_EQUIVALENT_DEFINITIONS.matcher(wordDefinitionStr);
         if (matcher.find()) {
-            //TODO:2021-05-09:yen: take a look at equal meaning E.g. `=`
-//            wordDefinition.setEqualMeaning();
+            wordDefinition.setIdiosyncraticMeaning(isNotBlank(matcher.group(1)));
+            wordDefinition.setEqualMeaning(isNotBlank(matcher.group(2)));
             //TODO:2021-05-09:yen: take a look at stylistic marker. E.g. `мат`
 //            wordDefinition.setStylisticMarker();
-            wordDefinition.setDefinition(matcher.group(1));
-            wordDefinition.setExample(matcher.group(2));
-            wordDefinition.setEquivalentDefinitions(equivalentWordDefinitions(matcher.group(3)));
+            wordDefinition.setDefinition(matcher.group(3));
+            wordDefinition.setExample(matcher.group(4));
+            wordDefinition.setEquivalentDefinitions(equivalentWordDefinitions(matcher.group(5)));
         }
         return wordDefinition;
     }
